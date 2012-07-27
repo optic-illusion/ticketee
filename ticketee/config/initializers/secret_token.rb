a6dede44c4275dbfaef24fe284e8f50495123312
optic-illusion <kelvin.mba@gmail.com>
2012-07-27 15:25:19 -0400

    Initial Commit

---
+++ ticketee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = '9e33a8a972917c06be76eb8c07ced76f95561ebd89d2d03c3af72ec07cf40eb05530b589be9fed012a84529bae83daaa018d2221b374060942e90564fe0847c5'
